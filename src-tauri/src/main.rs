da29ef153cb67dbe7bdaefdc8033e384e94edf3f
Adam McQuilkin <46639306+ajmcquilkin@users.noreply.github.com>
2022-10-23 23:19:43 -0400

    Added demo rust unit tests

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,28 @@
     windows_subsystem = "windows"
 )]
 
+// This function is included as a demo for unit testing
+// For more information, see the link below
+// https://doc.rust-lang.org/rust-by-example/testing/unit_testing.html
+pub fn add(a: i32, b: i32) -> i32 {
+    return a + b;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
+
+    #[test]
+    fn test_add_negative() {
+        assert_eq!(add(1, -2), -1);
+    }
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
