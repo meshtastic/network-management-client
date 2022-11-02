4912099607c00476fb79f91a36796018f12d7662
Adam McQuilkin <46639306+ajmcquilkin@users.noreply.github.com>
2022-11-02 13:23:09 -0400

    Added MVP scale

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import maplibregl from "maplibre-gl";
-import { Map } from "react-map-gl";
+import { Map, ScaleControl } from "react-map-gl";
 
 const MapBox = () => {
   return (
-    <Map
-      mapStyle="https://raw.githubusercontent.com/hc-oss/maplibre-gl-styles/master/styles/osm-mapnik/v8/default.json"
-      mapLib={maplibregl}
-      attributionControl={false}
-    ></Map>
+    <div className="relative w-full h-full">
+      <Map
+        mapStyle="https://raw.githubusercontent.com/hc-oss/maplibre-gl-styles/master/styles/osm-mapnik/v8/default.json"
+        mapLib={maplibregl}
+        attributionControl={false}
+      >
+        <ScaleControl position="bottom-right" unit="imperial" />
+      </Map>
+    </div>
   );
 };
 
