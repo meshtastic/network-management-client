9af3ad8ef9d0a00fa04cd08813d56505186d9d92
Sam Crombie <samuel.a.crombie.23@dartmouth.edu>
2022-11-01 22:32:09 -0400

    styling of sidebar and sidebar buttons

--- src/components/Sidebar/SidebarIcon.tsx
+++ src/components/Sidebar/SidebarIcon.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 
-export interface ISidebarIcon {
+export interface ISidebarIconProps {
   renderIcon: () => JSX.Element;
+  isActive: boolean;
+  setTabActive: () => void;
 }
 
-const SidebarIcon = (props: ISidebarIcon) => {
+const SidebarIcon = (props: ISidebarIconProps) => {
   return (
     <button
       type="button"
-      className=" hover:bg-blue-50 p-5"
+      className={`relative hover:bg-blue-50 w-14 h-14 ${
+        props.isActive ? "bg-blue-50" : ""
+      }`}
+      onClick={() => {
+        props.setTabActive();
+      }}
     >
-      <div className="opacity-50">{props.renderIcon()}</div>
+      <div className="opacity-50 m-4">{props.renderIcon()}</div>
+      {props.isActive && (
+        <span className="absolute bg-blue-400 h-full top-0 right-0 w-1" />
+      )}
     </button>
   );
 };
