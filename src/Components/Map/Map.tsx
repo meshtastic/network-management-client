b45d9e48fd5014bfdd2f6056a03c7a6e5a04fe88
Adam McQuilkin <46639306+ajmcquilkin@users.noreply.github.com>
2022-11-04 13:08:39 -0400

    Live position map nodes

--- src/Components/Map/Map.tsx
+++ src/Components/Map/Map.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import maplibregl from "maplibre-gl";
-import { Map, NavigationControl, ScaleControl } from "react-map-gl";
+import { Map, Marker, NavigationControl, ScaleControl } from "react-map-gl";
+
+import { selectAllDevices } from "@features/device/deviceSelectors";
 
 import 'maplibre-gl/dist/maplibre-gl.css';
 import "./Map.css";
 
 const MapBox = () => {
+  const devices = useSelector(selectAllDevices());
+
   return (
     <div className="relative w-full h-full">
       <Map
@@ -15,6 +20,16 @@ const MapBox = () => {
       >
         <ScaleControl maxWidth={144} position="bottom-right" unit="imperial" />
         <NavigationControl position="bottom-right" showCompass={false} />
+        {devices.map(d => (
+          <Marker
+            key={d.id}
+            rotation={(d.position?.groundTrack ?? 0) / 100}
+            latitude={d.position?.latitudeI ?? 0}
+            longitude={d.position?.longitudeI ?? 0}
+          >
+            Hi!
+          </Marker>
+        ))}
       </Map>
     </div>
   );
