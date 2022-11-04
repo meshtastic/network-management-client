524bf7df2f7e175cb40460788b251a21dfe6b1b3
BC <barkincavdar@gmail.com>
2022-11-03 22:39:57 -0400

    resolve the comments

--- src-tauri/src/algorithms/stoer_wagner.rs
+++ src-tauri/src/algorithms/stoer_wagner.rs
@@ -1,22 +1,21 @@
 #![allow(dead_code)]
-#![allow(non_snake_case)]
 
 use crate::aux_data_structures::{
     binary_heap::BinaryHeap, cut::Cut, stoer_wagner_ds::StoerWagnerGraph,
 };
 
-pub fn stMinCut(G: &mut StoerWagnerGraph) -> Option<Cut> {
-    let mut bheap = BinaryHeap::new(G);
+pub fn st_mincut(g: &mut StoerWagnerGraph) -> Option<Cut> {
+    let mut bheap = BinaryHeap::new(g);
 
-    let uncontracted = G.uncontracted.clone();
-    let mut S = Vec::new();
+    let uncontracted = g.uncontracted.clone();
+    let mut a = Vec::new();
 
-    while S.len() < uncontracted.clone().len() {
+    while a.len() < uncontracted.clone().len() {
         let max = bheap.extract_max().unwrap();
-        S.push(max.clone());
+        a.push(max.clone());
 
-        for node_id in G.uncontracted.clone() {
-            let w = G
+        for node_id in g.uncontracted.clone() {
+            let w = g
                 .graph
                 .get_edge_weight(node_id.clone(), max.node.name.clone(), None, None);
             bheap.update(node_id.clone(), w);
@@ -24,24 +23,24 @@ pub fn stMinCut(G: &mut StoerWagnerGraph) -> Option<Cut> {
         bheap.build_heap();
     }
 
-    let weight = S[S.len() - 1].weight;
-    if S.len() < 2 {
+    let weight = a[a.len() - 1].weight;
+    if a.len() < 2 {
         return None;
     }
 
-    let s = S[S.len() - 1].node.name.clone();
-    let t = S[S.len() - 2].node.name.clone();
+    let s = a[a.len() - 1].node.name.clone();
+    let t = a[a.len() - 2].node.name.clone();
 
     return Some(Cut::new(weight, s, t));
 }
 
-pub fn StoerWagner(graph: &mut StoerWagnerGraph) -> Cut {
+pub fn stoer_wagner(graph: &mut StoerWagnerGraph) -> Cut {
     if graph.uncontracted.len() == 2 {
         return graph.get_cut();
     } else {
-        let cut = stMinCut(graph).unwrap();
+        let cut = st_mincut(graph).unwrap();
         graph.contract_edge(cut.get_a().to_string(), cut.get_b().to_string());
-        let other_cut = StoerWagner(graph);
+        let other_cut = stoer_wagner(graph);
         if cut.get_weight() < other_cut.get_weight() {
             return cut;
         } else {
@@ -56,12 +55,11 @@ mod tests {
     use crate::graph::graph_ds::Graph;
 
     use super::*;
-    use std::collections::HashMap;
 
     #[test]
     fn test_stoer_wagner() {
         // Create a graph
-        let mut G = Graph::new();
+        let mut g = Graph::new();
 
         // Add nodes
         let u: String = "u".to_string();
@@ -73,41 +71,36 @@ mod tests {
         let a: String = "a".to_string();
         let b: String = "b".to_string();
 
-        G.add_node(u.clone());
-        G.add_node(v.clone());
-        G.add_node(w.clone());
-        G.add_node(x.clone());
-        G.add_node(y.clone());
-        G.add_node(z.clone());
-        G.add_node(a.clone());
-        G.add_node(b.clone());
+        g.add_node(u.clone());
+        g.add_node(v.clone());
+        g.add_node(w.clone());
+        g.add_node(x.clone());
+        g.add_node(y.clone());
+        g.add_node(z.clone());
+        g.add_node(a.clone());
+        g.add_node(b.clone());
 
         // Add edges
-        G.add_edge(u.clone(), v.clone(), 1.0);
-        G.add_edge(u.clone(), w.clone(), 1.0);
-        G.add_edge(u.clone(), x.clone(), 1.0);
-        G.add_edge(w.clone(), x.clone(), 1.0);
-        G.add_edge(v.clone(), y.clone(), 1.0);
-        G.add_edge(x.clone(), y.clone(), 1.0);
-        G.add_edge(w.clone(), z.clone(), 1.0);
-        G.add_edge(x.clone(), a.clone(), 1.0);
-        G.add_edge(y.clone(), b.clone(), 1.0);
+        g.add_edge(u.clone(), v.clone(), 1.0);
+        g.add_edge(u.clone(), w.clone(), 1.0);
+        g.add_edge(u.clone(), x.clone(), 1.0);
+        g.add_edge(w.clone(), x.clone(), 1.0);
+        g.add_edge(v.clone(), y.clone(), 1.0);
+        g.add_edge(x.clone(), y.clone(), 1.0);
+        g.add_edge(w.clone(), z.clone(), 1.0);
+        g.add_edge(x.clone(), a.clone(), 1.0);
+        g.add_edge(y.clone(), b.clone(), 1.0);
 
-        let mut counter_hashmap = HashMap::new();
+        let mut correct_mincut_weight_count = 0;
 
         for _ in 0..100 {
-            let graph_sw = &mut StoerWagnerGraph::new(G.clone());
-            let mincut = StoerWagner(graph_sw);
-            // increase the number of times mincut occurs in counter_hashmap
-            let counter = counter_hashmap
-                .entry(mincut.get_weight() as i32)
-                .or_insert(0);
-            *counter += 1;
+            let graph_sw = &mut StoerWagnerGraph::new(g.clone());
+            let mincut = stoer_wagner(graph_sw);
+            if mincut.get_weight() == 1.0 {
+                correct_mincut_weight_count += 1;
+            }
         }
 
-        println!("Counter hashmap: {:?}", counter_hashmap);
-
-        assert_eq!(counter_hashmap.len(), 1);
-        assert_eq!(counter_hashmap.get(&1), Some(&100));
+        assert_eq!(correct_mincut_weight_count, 100);
     }
 }
