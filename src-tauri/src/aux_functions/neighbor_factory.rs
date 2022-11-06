15041873d91c6401485495e888af5fb6797167ef
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2022-11-06 09:10:01 -0500

    updated neighbor info, working on graph initialization

--- src-tauri/src/aux_functions/neighbor_factory.rs
+++ src-tauri/src/aux_functions/neighbor_factory.rs
@@ -3,70 +3,46 @@ use crate::graph::graph_ds::Graph;
 use crate::graph::node::Node;
 use app::protobufs::{Data, NeighborInfo, Position, User};
 use petgraph::graph::NodeIndex;
+use 
 
 // Take in data from the frontend in protobuf form, parse it, run algorithms on it, and
 // send it back.
 
-#[tauri::command]
-pub fn run_algorithm(data: Vec<Data>) -> Result<Data, String> {
+#[tauri::command]`
+pub fn run_algorithm(data: Vec<NeighborInfo>, algorithm: String) -> Result<Vec<Point>, String> {
     // Create a graph
-    // Run the specified algorithm on the graph
+    let mut graph = populate_graph(data);
+    
+    // Run the specified algorithm on the graph (based on the input)
+    let result = articulation_point(graph);
+
     // Return the result of the algorithm to the frontend in the specified data structure
-
     // We need to define the data fields of the response values (simply a point cloud?)
-
-    // let mut graph = Graph::new();
-
-    // // Add nodes
-    // for node in data {
-    //     let node_name = node.name;
-    //     let node_position = node.position.unwrap();
-    //     let node_position_x = node_position.x;
-    //     let node_position_y = node_position.y;
-    //     let node_position_z = node_position.z;
-    //     let node_position = Position {
-    //         x: node_position_x,
-    //         y: node_position_y,
-    //         z: node_position_z,
-    //     };
-    //     let node_position = Some(node_position);
-    //     let node = Node {
-    //         name: node_name,
-    //         position: node_position,
-    //     };
-    //     graph.add_node(node);
-    // }
-
-    // // Add edges
-    // for node in data {
-    //     let node_name = node.name;
-    //     let node_neighbors = node.neighbors.unwrap();
-    //     for neighbor in node_neighbors {
-    //         let neighbor_name = neighbor.name;
-    //         let neighbor_weight = neighbor.weight;
-    //         let edge = Edge {
-    //             name: neighbor_name,
-    //             weight: neighbor_weight,
-    //         };
-    //         graph.add_edge(node_name.clone(), edge);
-    //     }
-    // }
-
-    // // Run the algorithm
-    // let result = articulation_point(graph);
-
-    // // Convert the result to protobuf form
-    // let mut result_data = Data::new();
-    // let mut result_data_neighbors = Vec::new();
-    // for node in result {
-    //     let mut result_data_neighbor = NeighborInfo::new();
-    //     result_data_neighbor.name = node.name;
-    //     result_data_neighbors.push(result_data_neighbor);
-    // }
-    // result_data.neighbors = Some(result_data_neighbors);
-
-    // Ok(result_data)
+    Vec<Point> result = Vec<Point>::new();
+    result = create_point_cloud_from_graph(graph);
+    return result;
 }
+
+
+pub fn create_point_cloud_from_graph(graph: Graph) -> Vec<Point> {
+    // Create a point cloud from the graph
+    // Return the point cloud
+    Vec<Point> point_cloud = Vec<Point>::new();
+    for node in graph.get_nodes() {
+        let node_position = node.position.unwrap();
+        let node_position_x = node_position.x;
+        let node_position_y = node_position.y;
+        let node_position_z = node_position.z;
+        let point = Point {
+            x: node_position_x,
+            y: node_position_y,
+            z: node_position_z,
+        };
+        point_cloud.push(point);
+    }
+    return point_cloud;
+}
+
 
 /*
 * When we create a graph, we need a list of all nodes and each of their neighbor lists.
@@ -75,22 +51,43 @@ pub fn run_algorithm(data: Vec<Data>) -> Result<Data, String> {
 *
 * We will use it to calculate the pythagorean distance between nodes, and use this to create their edge weights.
  */
-pub fn create_graph(data: Data) -> Graph {
-    let mut graph = Graph::new();
-    for node in data.nodes {
-        graph.add_node(node.id);
+pub fn populate_graph(data: Vec<NeighborInfo>) -> Graph {
+  let mut graph = Graph::new();
+  let mut edge_left_endpoints = Vec<NodeIndex>::new();
+  let mut edge_right_endpoints = Vec<NodeIndex>::new();
+  let mut distances = Vec<f64>::new();
+  let mut radio_quality = Vec<f64>::new();
+  // for each node we have info on, add it and all its neighbors to the graph
+  for neighbor_info in data {
+    // Add node
+    if !graph.contains_node(neighbor.name) {
+      let node = Node::new(neighbor_info.user.name, neighbor_info.user.position);
+      graph.add_node(node);
     }
-    for edge in data.edges {
-        graph.add_edge(edge.source, edge.target, edge.weight);
+    // Add neighbors
+    for neighbor in neighbor_info.neighbors {
+      if !graph.contains_node(neighbor.name) {
+        let neighbor_node = Node::new(neighbor.name, neighbor.position);
+        edge_left_endpoints.add(neighbor_node);
+        edge_right_endpoints.add(neighbor_info);
+        let edge = Edge::new(neighbor_node, neighbor.weight);
+        graph.add_edge(edge);
+      }
     }
-    return graph;
+  }
+  return graph;
 }
 
-// pass in protobufs and do things with them; e.g. run different algorithms with different data
-pub fn print_protobuf(data: Data) -> Result<Data, String> {
+// printing utility for testing
+fn print_protobuf(data: Data) -> Result<Data, String> {
     // let mut data = serde_protobuf::from_bytes::<Data>(&data).unwrap();
     println!("data: {:?}", data);
     return Ok(data);
+}
+
+fn print_graph(graph: Graph) -> Result<Graph, String> {
+    println!("graph: {:?}", graph.to_string());
+    return Ok(graph);
 }
 
 #[cfg(test)]
