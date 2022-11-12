9c216be607d1d478f59949ad9c4565cf89f4e8fb
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2022-11-12 01:16:40 -0500

    removed wkt from build

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -26,23 +26,6 @@ fn main() -> std::io::Result<()> {
 
     prost_build::compile_protos(&protos, &[protobufs_dir]).unwrap();
 
-    // let out = PathBuf::from(env::var("OUT_DIR").unwrap());
-    // let descriptor_file = out.join("descriptors.bin");
-    // let mut prost_build = prost_build::Config::new();
-    // prost_build
-    //     .type_attribute(".", "#[derive(serde::Serialize,serde::Deserialize)]")
-    //     .extern_path(".google.protobuf.Any", "::prost_wkt_types::Any")
-    //     .extern_path(".google.protobuf.Timestamp", "::prost_wkt_types::Timestamp")
-    //     .extern_path(".google.protobuf.Value", "::prost_wkt_types::Value")
-    //     .file_descriptor_set_path(&descriptor_file)
-    //     .compile_protos(&protos, &[protobufs_dir])
-    //     .unwrap();
-
-    // let descriptor_bytes = std::fs::read(descriptor_file).unwrap();
-
-    // let descriptor = FileDescriptorSet::decode(&descriptor_bytes[..]).unwrap();
-
-    // prost_wkt_build::add_serde(out, descriptor);
     tauri_build::build();
 
     Ok(())
