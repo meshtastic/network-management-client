81f7b9b2917d8604468f3722f6feb52804a7c6ef
BC <barkincavdar@gmail.com>
2022-12-15 22:47:06 +0300

    handle disc in timeline

--- src-tauri/src/aux_data_structures/timeline.rs
+++ src-tauri/src/aux_data_structures/timeline.rs
@@ -1,6 +1,10 @@
 // A data structure to represent the timeline of graph snapshots.
+use crate::aux_functions::handle_disc;
+use crate::aux_functions::take_snapshot::take_snapshot_of_graph;
+use crate::graph::graph_ds::Graph;
 use postgres::{Client, NoTls};
 
+// Created at the beginning of each rescue attempt.
 pub struct Timeline {
     snapshots: Vec<Graph>,
     client: Client,
@@ -15,11 +19,29 @@ impl Timeline {
     }
 
     pub fn add_snapshot(&mut self, snapshot: Graph) {
-        self.snapshots.push(snapshot);
+        // check if there is a disconnection
+        if self.snapshots.len() > 0 {
+            let last_snapshot = self.snapshots.last().unwrap();
+            let is_connected = handle_disc::check_connection(last_snapshot, &snapshot);
+            if !is_connected {
+                // write the current timeline to database, then clear the timeline
+                // and create another
+                self.write();
+                self.snapshots.clear();
+                // change self to a new timeline
+                self.snapshots.push(snapshot);
+            } else {
+                self.snapshots.push(snapshot);
+            }
+        }
     }
 
     pub fn get_snapshots(&self) -> Vec<Graph> {
-        self.snapshots.clone()
+        let mut snapshots = Vec::new();
+        for snapshot in &self.snapshots {
+            snapshots.push(snapshot.clone());
+        }
+        snapshots
     }
 
     pub fn get_snapshot(&self, idx: usize) -> Graph {
@@ -30,7 +52,7 @@ impl Timeline {
         self.snapshots.len()
     }
 
-    pub fn write(&self) {
+    pub fn write(&mut self) {
         for snapshot in &self.snapshots {
             let snapshot_string = take_snapshot_of_graph(snapshot);
             let query = format!(
@@ -39,5 +61,9 @@ impl Timeline {
             );
             self.client.execute(query.as_str(), &[]).unwrap();
         }
+    }
+
+    pub fn clear(&mut self) {
+        self.snapshots.clear();
     }
 }
