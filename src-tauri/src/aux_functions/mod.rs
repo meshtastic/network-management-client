a945f17c3c9c726009abf1f4c2a6d87657c24e32
BC <barkincavdar@gmail.com>
2022-12-16 18:16:12 +0300

    clean up

--- src-tauri/src/aux_functions/mod.rs
+++ src-tauri/src/aux_functions/mod.rs
@@ -3,5 +3,4 @@ pub(crate) mod convert_to_graph_from_string;
 pub(crate) mod edge_factory;
 pub(crate) mod get_most_similar;
 pub(crate) mod graph_init;
-pub(crate) mod handle_disc;
 pub(crate) mod take_snapshot;
