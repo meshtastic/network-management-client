e9d6640732be387033014c421f83245f4b38f519
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2023-01-10 11:13:23 -0500

    hanover prefixes and setup

--- src-tauri/src/mocks/mock_generator.rs
+++ src-tauri/src/mocks/mock_generator.rs
@@ -1,7 +1,21 @@
+use crate::aux_data_structures::neighbor_info::NeighborInfo;
+/*
+Append two GPS decimal points to beginning of randomly generated latitude and longitude to simulate a moving network
+within the Hanover area.
+GPS precision is as follows:
+1 decimal point - 11.1 km
+2 decimal points - 1.11 km
+3 decimal points - 110 meters
+4 decimal points - 11 meters
+See https://gis.stackexchange.com/questions/8650/measuring-accuracy-of-latitude-and-longitude/8674#8674
+*/
+
+pub const HANOVER_LON_PREFIX: i32 = 43.70;
+pub const HANOVER_LAT_PREFIX: i32 = 72.28;
+
 /*
 Generates mock protobufs of struct types below
 */
-
 pub fn mock_generator(num_nodes: i32) -> neighborinfo_protobuf::NeighborInfo {
     let mut neighborinfo = neighborinfo_protobuf::NeighborInfo::new();
     neighborinfo.set_selfnode(mock_proto_node());
@@ -10,6 +24,12 @@ pub fn mock_generator(num_nodes: i32) -> neighborinfo_protobuf::NeighborInfo {
         num_nodes,
     ]));
     neighborinfo
+}
+
+pub fn mock_datastream(num_nodes: i32) -> neighborinfo_protobuf::NeighborInfo {
+    /*
+    Call mock generator in a loop with the same number of nodes to simulate a datastream from a moving network
+    */
 }
 
 #[cfg(test)]
