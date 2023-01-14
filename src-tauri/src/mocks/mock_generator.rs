e4e8aba071ec7bcef3de3866c00144a418dc12ad
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2023-01-13 21:30:47 -0500

    docstring updates

--- src-tauri/src/mocks/mock_generator.rs
+++ src-tauri/src/mocks/mock_generator.rs
@@ -2,18 +2,11 @@ use crate::aux_data_structures::neighbor_info::{Neighbor, NeighborInfo};
 use rand::prelude::*;
 use rand::seq::SliceRandom;
 /*
-Append two GPS decimal points to beginning of randomly generated latitude and longitude to simulate a moving network
-within the Hanover area.
-GPS precision is as follows:
-1 decimal point - 11.1 km
-2 decimal points - 1.11 km
-3 decimal points - 110 meters
-4 decimal points - 11 meters
-See https://gis.stackexchange.com/questions/8650/measuring-accuracy-of-latitude-and-longitude/8674#8674
-*/
+Generate a randomly connected graph with the degree of connectedness specified by percent_connected.
 
-pub const HANOVER_LON_PREFIX: f64 = 43.70;
-pub const HANOVER_LAT_PREFIX: f64 = 72.28;
+Note: this graph does not currently have location info attached. Location info will be retrieved
+by mapping the node id to its location info.
+*/
 
 pub fn mock_generator(num_nodes: i32, percent_connected: f64) -> Vec<NeighborInfo> {
     let mut neighborinfo_vec = Vec::new();
