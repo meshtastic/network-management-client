eee0873973f9daf309f2d4785ef0326aa71930fd
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2023-01-15 14:53:48 -0500

    hanover prefixes for mocks

--- src-tauri/src/aux_functions/conversion_factors.rs
+++ src-tauri/src/aux_functions/conversion_factors.rs
@@ -6,3 +6,16 @@ pub const LAT_CONVERSION_FACTOR: f64 = 1e-7;
 pub const LON_CONVERSION_FACTOR: f64 = 1e-7;
 /* Altitude: in meters above sea level, no conversion needed */
 pub const ALT_CONVERSION_FACTOR: f64 = 1.0;
+
+/*
+Append two GPS decimal points to beginning of randomly generated latitude and longitude to simulate a moving network
+within the Hanover area.
+GPS precision is as follows:
+1 decimal point - 11.1 km
+2 decimal points - 1.11 km
+3 decimal points - 110 meters
+4 decimal points - 11 meters
+See https://gis.stackexchange.com/questions/8650/measuring-accuracy-of-latitude-and-longitude/8674#8674
+*/
+pub const HANOVER_LON_PREFIX: f64 = 43.70;
+pub const HANOVER_LAT_PREFIX: f64 = 72.28;
