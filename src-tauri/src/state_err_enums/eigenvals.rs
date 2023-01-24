40c3f1057a1cd0e8ea35b9806357fa57e01c6b7e
BC <barkincavdar@gmail.com>
2023-01-24 11:33:46 -0500

    enum (more in future) for eigens: return eigenvals if success else return error message

---
+++ src-tauri/src/state_err_enums/eigenvals.rs
@@ -0,0 +1,14 @@
+#[derive(Debug)]
+pub enum EigenvalsResult {
+    Success(Vec<f64>),
+    Error(String),
+}
+
+impl Clone for EigenvalsResult {
+    fn clone(&self) -> Self {
+        match self {
+            EigenvalsResult::Success(eigenvals) => EigenvalsResult::Success(eigenvals.clone()),
+            EigenvalsResult::Error(err) => EigenvalsResult::Error(err.clone()),
+        }
+    }
+}
