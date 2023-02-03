9f32a66609dbb1c3cd7dc83fbd1d99a2dd81a9ce
Sherry Liu <57198797+sherry1iu@users.noreply.github.com>
2023-02-03 00:44:29 -0500

    Expanded sidebar implemented

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,59 +1,121 @@
-import React from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+// import { Provider, connect, useSelector, useDispatch } from "react-redux";
+
+import { ExpandedIcon } from "@components/Sidebar/SidebarOpen";
+import SidebarCollapsed from "@components/Sidebar/SidebarCollapsed";
+import tabList, { tabGroup } from "@components/Sidebar/TabList";
+
+// import LogoWhiteSVG from "@app/assets/Mesh_Logo_White.svg";
 
 import {
+  // Overview
+  MapIcon,
   ChatBubbleBottomCenterTextIcon,
-  MagnifyingGlassIcon,
-  DocumentTextIcon,
-  Cog8ToothIcon,
+
+  // Network
+  CircleStackIcon,
+  MapPinIcon,
+
+  // Config
   RadioIcon,
+  CubeIcon,
+  EnvelopeIcon,
+
+  // Settings
+  DocumentArrowDownIcon,
+  Cog8ToothIcon,
+  ArrowLeftOnRectangleIcon,
+
+  // Alt Collapse
+  ChevronDoubleLeftIcon,
+  ChevronDoubleRightIcon,
+
+  // Testing
+  BeakerIcon,
 } from "@heroicons/react/24/outline";
 
-import LogoWhiteSVG from "@app/assets/Mesh_Logo_White.svg";
-import SidebarIcon from "@components/Sidebar/SidebarIcon";
+// TODO:
+// 1. Sidebar should be overlaid instead of on the Side; on opening, it should not scrunch the map over
+//    On this point; there's something wrong with messaging. I'm guessing it's because Message flex is overwriting the sidebar size somehow? But
+//    If we make this floating then the messaging shouldn't bother it.
+// 2. Add in an overflow because the sidebar very long
+// 3. Propose new buttons
+// 5. Clean, clear documentation
 
 const Sidebar = () => {
-  const location = useLocation();
-  const navigateTo = useNavigate();
+  const [isSidebarExpand, setIsSidebarExpand] = useState(true);
 
   return (
-    <div className="h-screen flex flex-col justify-between shadow-lg">
-      <div>
-        <div className="flex bg-gray-800 w-14 h-14">
-          <img src={LogoWhiteSVG} className="m-auto pl-2 pr-2 pt-3 pb-3"></img>{" "}
-        </div>
-        <div className="flex flex-col">
-          <SidebarIcon
-            isActive={location.pathname === "/" && !location.hash}
-            onClick={() => navigateTo("/")}
-            renderIcon={() => <MagnifyingGlassIcon className="w-6" />}
-          />
-          <SidebarIcon
-            isActive={
-              location.pathname === "/messaging"
-            }
-            onClick={() => navigateTo("/messaging")}
-            renderIcon={() => (
-              <ChatBubbleBottomCenterTextIcon className="w-6" />
-            )}
-          />
-          <SidebarIcon
-            isActive={location.pathname === "/info"}
-            onClick={() => navigateTo("/info")}
-            renderIcon={() => <DocumentTextIcon className="w-6" />}
-          />
-          <SidebarIcon
-            isActive={location.pathname === "/serial-connect"}
-            onClick={() => navigateTo("/serial-connect")}
-            renderIcon={() => <RadioIcon className="w-6" />}
-          />
-        </div>
+    // Flex col tells it that we want them aligned vertically
+    <div className="bg-white text-base flex flex-col overflow-y-auto h-screen">
+      <div className="justify-start">
+        {isSidebarExpand ? (
+          <div>
+            <div className="px-3 text-xs text-gray-500 font-semibold pr-2 w-62">
+              OPEN
+              <div className="py-2">
+                <div>
+                  OVERVIEW
+                  {ExpandedIcon("View Map", "/", <MapIcon />)}
+                  {ExpandedIcon(
+                    "Messaging",
+                    "/messaging",
+                    <ChatBubbleBottomCenterTextIcon />
+                  )}
+                </div>
+                <div>
+                  NETWORK
+                  {ExpandedIcon("Manage Nodes", "/TODO", <CircleStackIcon />)}
+                  {ExpandedIcon("Manage Waypoints", "/TODO", <MapPinIcon />)}
+                </div>
+                <div>
+                  CONFIGURATION
+                  {ExpandedIcon(
+                    "Radio Configuration",
+                    "/serial-connect",
+                    <RadioIcon />
+                  )}
+                  {ExpandedIcon("Plugin Configuration", "/TODO", <CubeIcon />)}
+                  {ExpandedIcon(
+                    "Channel Configuration",
+                    "/TODO",
+                    <EnvelopeIcon />
+                  )}
+                </div>
+                <div>
+                  SETTINGS
+                  {ExpandedIcon(
+                    "Export Data",
+                    "/TODO",
+                    <DocumentArrowDownIcon />
+                  )}
+                  {ExpandedIcon(
+                    "Application Settings",
+                    "/#settings",
+                    <Cog8ToothIcon />
+                  )}
+                  {ExpandedIcon(
+                    "Collapse Sidebar",
+                    "/TODO",
+                    <ArrowLeftOnRectangleIcon />
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div>hi</div>
+        )}
       </div>
-      <SidebarIcon
-        isActive={location.pathname === "/" && location.hash === "#settings"}
-        onClick={() => navigateTo("/#settings")}
-        renderIcon={() => <Cog8ToothIcon className="w-6" />}
-      />
+
+      <div className="flex justify-self-end ">
+        <button
+          className="bg-rose-600"
+          onClick={() => setIsSidebarExpand(!isSidebarExpand)}
+        >
+          Change
+        </button>
+      </div>
     </div>
   );
 };
