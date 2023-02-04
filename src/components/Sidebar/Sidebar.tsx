d886308266ac604495f3b407f71f796bd0551dd2
Sherry Liu <57198797+sherry1iu@users.noreply.github.com>
2023-02-04 17:25:23 -0500

    initial sidebar redo

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 // import { Provider, connect, useSelector, useDispatch } from "react-redux";
 
-import { ExpandedIcon } from "@components/Sidebar/SidebarOpen";
-import SidebarCollapsed from "@components/Sidebar/SidebarCollapsed";
-import tabList, { tabGroup } from "@components/Sidebar/TabList";
+import { ExpandedIcon, CollapsedIcon } from "@components/Sidebar/SidebarIcon";
+// import SidebarCollapsed from "@components/Sidebar/SidebarCollapsed";
+// import tabList, { tabGroup } from "@components/Sidebar/TabList";
+
+import { useLocation, useNavigate } from "react-router-dom";
 
 // import LogoWhiteSVG from "@app/assets/Mesh_Logo_White.svg";
 
@@ -38,73 +40,91 @@ import {
 // 1. Sidebar should be overlaid instead of on the Side; on opening, it should not scrunch the map over
 //    On this point; there's something wrong with messaging. I'm guessing it's because Message flex is overwriting the sidebar size somehow? But
 //    If we make this floating then the messaging shouldn't bother it.
-// 2. Add in an overflow because the sidebar very long
+//    In this vein; where did we add the sidebar in? The reason it's doing this is because of clearly defined sizes in Settings.
 // 3. Propose new buttons
 // 5. Clean, clear documentation
+// 6. Remove the #messaging and #settings from main menu
 
 const Sidebar = () => {
   const [isSidebarExpand, setIsSidebarExpand] = useState(true);
+  // Icon for expanded sidebar
 
   return (
     // Flex col tells it that we want them aligned vertically
     <div className="bg-white text-base flex flex-col overflow-y-auto h-screen">
       <div className="justify-start">
         {isSidebarExpand ? (
-          <div>
-            <div className="px-3 text-xs text-gray-500 font-semibold pr-2 w-62">
-              OPEN
-              <div className="py-2">
-                <div>
-                  OVERVIEW
-                  {ExpandedIcon("View Map", "/", <MapIcon />)}
-                  {ExpandedIcon(
-                    "Messaging",
-                    "/messaging",
-                    <ChatBubbleBottomCenterTextIcon />
-                  )}
-                </div>
-                <div>
-                  NETWORK
-                  {ExpandedIcon("Manage Nodes", "/TODO", <CircleStackIcon />)}
-                  {ExpandedIcon("Manage Waypoints", "/TODO", <MapPinIcon />)}
-                </div>
-                <div>
-                  CONFIGURATION
-                  {ExpandedIcon(
-                    "Radio Configuration",
-                    "/serial-connect",
-                    <RadioIcon />
-                  )}
-                  {ExpandedIcon("Plugin Configuration", "/TODO", <CubeIcon />)}
-                  {ExpandedIcon(
-                    "Channel Configuration",
-                    "/TODO",
-                    <EnvelopeIcon />
-                  )}
-                </div>
-                <div>
-                  SETTINGS
-                  {ExpandedIcon(
-                    "Export Data",
-                    "/TODO",
-                    <DocumentArrowDownIcon />
-                  )}
-                  {ExpandedIcon(
-                    "Application Settings",
-                    "/#settings",
-                    <Cog8ToothIcon />
-                  )}
-                  {ExpandedIcon(
-                    "Collapse Sidebar",
-                    "/TODO",
-                    <ArrowLeftOnRectangleIcon />
-                  )}
-                </div>
+          <div className="px-5 text-xs text-gray-500 font-semibold w-62">
+            OPEN
+            <div className="py-2">
+              <div>
+                OVERVIEW
+                {ExpandedIcon("View Map", "/", <MapIcon />)}
+                {ExpandedIcon(
+                  "Messaging",
+                  "/messaging",
+                  <ChatBubbleBottomCenterTextIcon />
+                )}
+              </div>
+              <div>
+                NETWORK
+                {ExpandedIcon("Manage Nodes", "/TODO", <CircleStackIcon />)}
+              </div>
+              <div>
+                CONFIGURATION
+                {ExpandedIcon(
+                  "Radio Configuration",
+                  "/serial-connect",
+                  <RadioIcon />
+                )}
+              </div>
+              <div>
+                SETTINGS
+                {ExpandedIcon(
+                  "Application Settings",
+                  "/settings",
+                  <Cog8ToothIcon />
+                )}
               </div>
             </div>
           </div>
         ) : (
-          <div>hi</div>
+          // If isSidebarExpand is False
+          <div className="px-5 text-xs text-gray-500 font-semibold w-62">
+            Closed
+            <div className="py-2">
+              <div>
+                ...
+                <br />
+                {CollapsedIcon("Miew Vap", "/", <MapIcon />)}
+                {CollapsedIcon(
+                  "Saggingmess",
+                  "/messaging",
+                  <ChatBubbleBottomCenterTextIcon />
+                )}
+              </div>
+              <div>
+                ...
+                {CollapsedIcon("Nanage Modes", "/TODO", <CircleStackIcon />)}
+              </div>
+              <div className="">
+                ...
+                {CollapsedIcon(
+                  "Radio Configuration",
+                  "/serial-connect",
+                  <RadioIcon />
+                )}
+              </div>
+              <div>
+                ...
+                {CollapsedIcon(
+                  "Application Settings",
+                  "/settings",
+                  <Cog8ToothIcon />
+                )}
+              </div>
+            </div>
+          </div>
         )}
       </div>
 
--- src/components/Sidebar/SidebarIcon.tsx
+++ src/components/Sidebar/SidebarIcon.tsx
@@ -1,26 +1,86 @@
 import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
-export interface ISidebarIconProps {
-  isActive: boolean;
-  renderIcon: () => JSX.Element;
-  onClick: () => void;
-}
+// This file contains the code for each individual tab in the sidebar.
+// There are two forms: one for each of when the sidebar is expanded and collapsed.
+// Both forms take in 3 props:
+//    1. The name of the tab
+//    2. The path name for the tab
+//    3. The SVG icon for the tab
+//
 
-const SidebarIcon = ({ isActive, renderIcon, onClick }: ISidebarIconProps) => {
+const ExpandedIcon = (name: string, pathname: string, icon: JSX.Element) => {
+  // Icon for expanded sidebar
+
+  const navigateTo = useNavigate();
+  const isActive = useLocation().pathname === pathname;
+
   return (
-    <button
-      type="button"
-      className={`relative hover:bg-blue-50 w-14 h-14 ${
-        isActive ? "bg-blue-50" : ""
-      }`}
-      onClick={onClick}
-    >
-      <div className="flex text-gray-500 m-4">{renderIcon()}</div>
-      {isActive && (
-        <span className="absolute bg-blue-400 h-full top-0 right-0 w-1" />
-      )}
-    </button>
+    <div className="">
+      {/* Controls background color when clicked */}
+      <div
+        className={`px-2 flex flex-none rounded-lg font-semibold text-base
+        ${isActive ? "bg-gray-700" : "bg-white"} `}
+      >
+        {/* Sets entire horizontal area as tab */}
+        <button
+          className="flex flex-row w-full py-3"
+          onClick={() => navigateTo(pathname)}
+        >
+          {/* Display the icon */}
+          <div
+            className={`px-1 w-9 ${
+              isActive ? "text-gray-200" : "text-gray-400"
+            }`}
+          >
+            {icon}
+          </div>
+
+          {/* Display the name of the tab */}
+          <div
+            className={`px-2 self-center
+            ${isActive ? "text-gray-100" : "text-gray-600"}
+            `}
+          >
+            {name}
+          </div>
+        </button>
+      </div>
+    </div>
   );
 };
 
-export default SidebarIcon;
+const CollapsedIcon = (name: string, pathname: string, icon: JSX.Element) => {
+  // Icon for collapsed sidebar
+
+  const navigateTo = useNavigate();
+  const isActive = useLocation().pathname === pathname;
+
+  return (
+    <div className="">
+      {/* Controls background color when clicked */}
+      <div
+        className={`px-2 flex flex-none rounded-lg font-semibold text-base
+        ${isActive ? "bg-gray-700" : "bg-white"} `}
+      >
+        {/* Sets entire horizontal area as tab */}
+        <button
+          className="flex flex-row w-full py-3"
+          onClick={() => navigateTo(pathname)}
+        >
+          {/* Display the icon */}
+          <div
+            className={`px-1 w-9 ${
+              isActive ? "text-gray-200" : "text-gray-400"
+            }`}
+          >
+            {icon}
+          </div>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export { ExpandedIcon };
+export { CollapsedIcon };
