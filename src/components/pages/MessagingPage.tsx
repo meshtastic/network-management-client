24b6b5a356e7b554109e5af4505d8b6d19b640fc
Sherry Liu <57198797+sherry1iu@users.noreply.github.com>
2023-02-06 18:21:41 -0500

    Fit settings, messages to size

--- src/components/pages/MessagingPage.tsx
+++ src/components/pages/MessagingPage.tsx
@@ -1,34 +1,43 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
-import { Cog6ToothIcon } from '@heroicons/react/24/outline';
-import NavigationBacktrace from '@components/NavigationBacktrace';
-import ChannelListElement from '@components/Messaging/ChannelListElement';
-import { selectDeviceChannels } from '@features/device/deviceSelectors';
+import { Cog6ToothIcon } from "@heroicons/react/24/outline";
+import NavigationBacktrace from "@components/NavigationBacktrace";
+import ChannelListElement from "@components/Messaging/ChannelListElement";
+import { selectDeviceChannels } from "@features/device/deviceSelectors";
 
 const MessagingPage = () => {
   const channels = useSelector(selectDeviceChannels());
   const [activeChannel, setActiveChannel] = useState<number | null>(null);
 
   return (
-    <div className='flex flex-row w-full height-full'>
-      <div className='flex flex-col w-96 shadow-lg'>
-        <div className='flex justify-center align-middle px-9 h-20 border-b border-gray-100'>
-          <NavigationBacktrace className='my-auto mr-auto' levels={["Messaging"]} />
+    <div className="flex flex-row w-fill height-full">
+      <div className="flex flex-col w-96 shadow-lg">
+        <div className="flex justify-center align-middle px-9 h-20 border-b border-gray-100">
+          <NavigationBacktrace
+            className="my-auto mr-auto"
+            levels={["Messaging"]}
+          />
         </div>
 
-        <div className='flex flex-col flex-1 px-9 py-6'>
-          <div className='flex flex-row justify-between align-middle mb-6'>
-            <h1 className='text-4xl leading-10 font-semibold text-gray-700'>Messaging</h1>
-            <button type="button" className='cursor-pointer' onClick={() => alert('not implemented')}>
-              <Cog6ToothIcon className='w-6 h-6 text-gray-400 my-auto' />
+        <div className="flex flex-col flex-1 px-9 py-6">
+          <div className="flex flex-row justify-between align-middle mb-6">
+            <h1 className="text-4xl leading-10 font-semibold text-gray-700">
+              Messaging
+            </h1>
+            <button
+              type="button"
+              className="cursor-pointer"
+              onClick={() => alert("not implemented")}
+            >
+              <Cog6ToothIcon className="w-6 h-6 text-gray-400 my-auto" />
             </button>
           </div>
 
-          <div className='flex flex-col flex-1 gap-3'>
+          <div className="flex flex-col flex-1 gap-3">
             {channels
-              .filter(c => c.config.role !== 0) // * ignore DISABLED role
-              .map(c => (
+              .filter((c) => c.config.role !== 0) // * ignore DISABLED role
+              .map((c) => (
                 <ChannelListElement
                   key={c.config.index}
                   setActiveChannel={setActiveChannel}
@@ -39,7 +48,7 @@ const MessagingPage = () => {
           </div>
         </div>
       </div>
-      <div className='flex flex-1'>Other stuff</div>
+      <div className="flex flex-1">Other stuff</div>
     </div>
   );
 };
