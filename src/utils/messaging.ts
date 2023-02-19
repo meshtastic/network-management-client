dababe063ec4b13402ef0bcddae6ad4a11a84f3d
Adam McQuilkin <46639306+ajmcquilkin@users.noreply.github.com>
2023-02-18 21:49:25 -0500

    Displayed title for unnamed channels

--- src/utils/messaging.ts
+++ src/utils/messaging.ts
@@ -2,7 +2,7 @@ import type { MeshChannel } from "@bindings/MeshChannel";
 
 export const getChannelName = (channel: MeshChannel): string => {
   if (channel.config.role === 1) return "Primary";
-  return channel.config.settings?.name ?? "Unnamed Channel";
+  return channel.config.settings?.name || "Unnamed Channel";
 };
 
 export const formatMessageUsername = (
