3d3936aea0a7d5b63267a695904b963010b69d7d
Sherry Liu <57198797+sherry1iu@users.noreply.github.com>
2023-02-27 21:31:36 -0500

    Updated config tables

--- src/components/pages/ManageWaypointPage.tsx
+++ src/components/pages/ManageWaypointPage.tsx
@@ -1,22 +1,30 @@
 import React, { useMemo } from "react";
 import { useSelector } from "react-redux";
 import type { ColumnDef } from "@tanstack/react-table";
+import moment from "moment";
 
 import type { Waypoint } from "@bindings/protobufs/Waypoint";
 import TableLayout from "@components/Table/TableLayout";
-import { selectAllWaypoints } from "@features/device/deviceSelectors";
+import { selectAllWaypoints, selectUserByNodeId } from "@features/device/deviceSelectors";
 
 const ManageWaypointPage = () => {
   const waypoints = useSelector(selectAllWaypoints());
+
   const columns = useMemo<ColumnDef<Waypoint, unknown>[]>(
     () => [
       { header: "ID", accessorKey: "id" },
-      { header: "Name", accessorKey: "name" },
-      { header: "Description", accessorKey: "description" },
-      { header: "Expires", accessorKey: "expire" },
-      { header: "Latitude", accessorKey: "latitudeI" },
-      { header: "Longitude", accessorKey: "longitudeI" },
-      { header: "Owned by", accessorKey: "lockedTo" },
+      {id: "Name", accessorFn: (n) => (n.name!=="" ? n.name : "N/A"),},
+      {id: "Description", accessorFn: (n) => (n.description!=="" ? n.description : "N/A"),},
+      {id: "Expires", accessorFn: (n) => moment.unix(n.expire)},
+      {
+        id: "Latitude",
+        accessorFn: (n) => (n.latitudeI || n.latitudeI==0) ? n.latitudeI/1e7 : "Latitude Unavailable",
+      },
+      {
+        id: "Longitude",
+        accessorFn: (n) => (n.longitudeI || n.longitudeI==0) ? (n.longitudeI/1e7) : "Longitude Unavailable",
+      },
+      {id: "Owned by", accessorFn:(n) => (n.lockedTo==0 ? "Public" : useSelector(selectUserByNodeId(n.lockedTo)) ? useSelector(selectUserByNodeId(n.lockedTo))?.longName : "N/A")}
     ],
     [waypoints]
   );
