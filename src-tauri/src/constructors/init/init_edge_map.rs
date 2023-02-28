e09441339b7290973ff63e3cdaeba3e6ed7da576
uhuruhashimoto <uhuru.g.hashimoto.23@dartmouth.edu>
2023-02-27 21:36:43 -0500

    handling dropped edges successfully even if old records persist in nodedbs

--- src-tauri/src/constructors/init/init_edge_map.rs
+++ src-tauri/src/constructors/init/init_edge_map.rs
@@ -17,7 +17,8 @@ pub fn init_edge_map(neighbors: HashMap<u32, NeighborInfo>) -> HashMap<(u32, u32
                 as_key(node_1, node_2),
                 (neighbor.snr as f64, neighbor.rx_time as u64),
             );
-            let opposite_neighbor = get_corresponding_neighbor(node_2, node_1, &neighbors);
+            // If the opposite neighbor is found on a recent packet, we take the most recent timestamp as fact.
+            let opposite_neighbor = get_other_edge_side_packet(node_2, node_1, &neighbors);
             if opposite_neighbor.is_some() {
                 let opposite_neighbor = opposite_neighbor.unwrap();
                 if neighbor.rx_time > opposite_neighbor.rx_time {
@@ -32,38 +33,21 @@ pub fn init_edge_map(neighbors: HashMap<u32, NeighborInfo>) -> HashMap<(u32, u32
                     );
                 }
             } else {
-                println!("Opposite neighbor not found for node {} and neighbor {}", node_1, node_2);
+                // If the opposite neighbor is not found on a recent packet, we remove the edge from the hashmap.
+                let opposite_node = neighbors.get(&node_2).unwrap();
+                if opposite_node.tx_time > neighbor_packet.tx_time {
+                    if snr_hashmap.contains_key(&as_key(node_1, node_2)) {
+                        snr_hashmap.remove(&as_key(node_1, node_2));
+                    }
+                }
             }
         }
     }
     snr_hashmap
 }
-    // for node_packet in packets {
-    //     let node_id = node_packet.id;
-    //     for neighbor_packet in node_packet.neighbors {
-    //         let neighbor_id = neighbor_packet.id;
-    //         // Check if the edge is already in the hashmap
-    //         let edge_exists = snr_hashmap.contains_key(&as_key(node_id, neighbor_id));
-    //         if edge_exists {
-    //             let edge_snr_time = snr_hashmap.get(&as_key(node_id, neighbor_id)).unwrap();
-    //             let hashmap_timestamp = edge_snr_time.1;
-    //             // Replace the edge if the new timestamp is more recent (larger)
-    //             if hashmap_timestamp < neighbor_packet.timestamp {
-    //                 snr_hashmap.insert(
-    //                     as_key(node_id, neighbor_id),
-    //                     (neighbor_packet.snr, neighbor_packet.timestamp),
-    //                 );
-    //             }
-    //         } else {
-    //             snr_hashmap.insert(
-    //                 as_key(node_id, neighbor_id),
-    //                 (neighbor_packet.snr, neighbor_packet.timestamp),
-    //             );
-    //         }
-    //     }
-    // }
 
-pub fn get_corresponding_neighbor(
+// pulls corresponding neighbor from a hashmap of NeighborInfo packets
+pub fn get_other_edge_side_packet(
     node_id: u32,
     neighbor_id: u32,
     neighbors: &HashMap<u32, NeighborInfo>,
@@ -179,5 +163,65 @@ mod tests {
         println!("snr_hashmap: {:?}", snr_hashmap);
         assert_eq!(snr_hashmap.len(), 1);
         assert_eq!(snr_hashmap.get(&as_key(1, 2)).unwrap().0, 2.0);
+    }
+
+    #[test]
+    pub fn test_edge_drop_off() {
+        let neighbor_1 = Neighbor {
+            node_id: 1,
+            rx_time: 1,
+            snr: 1.0,
+        };
+        let neighbor_2 = Neighbor {
+            node_id: 2,
+            rx_time: 2,
+            snr: 2.0,
+        };
+        let neighbor_3 = Neighbor {
+            node_id: 3,
+            rx_time: 3,
+            snr: 3.0,
+        };
+        let neighbor_4 = Neighbor {
+            node_id: 4,
+            rx_time: 4,
+            snr: 4.0,
+        };
+        // Start with a full set of neighbors
+        let neighbor_info_1 = NeighborInfo {
+            node_id: 1,
+            tx_time: 0,
+            neighbors: vec![neighbor_2.clone(), neighbor_3.clone(), neighbor_4.clone()],
+        };
+        let neighbor_info_2: NeighborInfo = NeighborInfo {
+            node_id: 2,
+            tx_time: 0,
+            neighbors: vec![neighbor_1.clone(), neighbor_3.clone(), neighbor_4.clone()],
+        };
+        let neighbor_info_3: NeighborInfo = NeighborInfo {
+            node_id: 3,
+            tx_time: 0,
+            neighbors: vec![neighbor_1.clone(), neighbor_2.clone(), neighbor_4.clone()],
+        };
+        // Drop a neighbor
+        let neighbor_info_4: NeighborInfo = NeighborInfo {
+            node_id: 4,
+            tx_time: 100,
+            neighbors: vec![neighbor_2.clone(), neighbor_3.clone()],
+        };
+        let neighborinfo_vec = vec![
+            neighbor_info_1.clone(),
+            neighbor_info_2.clone(),
+            neighbor_info_3.clone(),
+            neighbor_info_4.clone(),
+        ];
+        let mut neighborinfo_hashmap = HashMap::new();
+        for neighborinfo in neighborinfo_vec {
+            neighborinfo_hashmap.insert(neighborinfo.node_id, neighborinfo);
+        }
+        println!("neighborinfo_hashmap: {:?}", neighborinfo_hashmap);
+        let snr_hashmap = init_edge_map(neighborinfo_hashmap);
+        println!("snr_hashmap: {:?}", snr_hashmap);
+        assert_eq!(snr_hashmap.len(), 5);
     }
 }
