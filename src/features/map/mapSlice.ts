26a8d69a7225b999b12020496843aecf7fb8079a
Adam McQuilkin <46639306+ajmcquilkin@users.noreply.github.com>
2023-05-05 13:47:16 -0400

    Moved map style config into redux

--- src/features/map/mapSlice.ts
+++ src/features/map/mapSlice.ts
@@ -1,9 +1,14 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import type { ViewState } from "react-map-gl";
 
+export interface IMapConfig {
+  style: string;
+}
+
 export interface IMapState {
   viewState: Partial<ViewState>;
   edgesFeatureCollection: GeoJSON.FeatureCollection | null;
+  config: IMapConfig
 }
 
 export const initialMapState: IMapState = {
@@ -13,6 +18,9 @@ export const initialMapState: IMapState = {
     zoom: 0.0,
   },
   edgesFeatureCollection: null,
+  config: {
+    style: "https://raw.githubusercontent.com/hc-oss/maplibre-gl-styles/master/styles/osm-mapnik/v8/default.json"
+  }
 };
 
 export const mapSlice = createSlice({
