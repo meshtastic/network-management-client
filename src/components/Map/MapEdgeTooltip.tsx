70bed06d82f7094b063a0d6b6b64dd6c90e3c315
Matt Carroll <mc@qacafe.com>
2025-02-20 17:54:39 -0500

    Add z-inherit to mouse tooltips

--- src/components/Map/MapEdgeTooltip.tsx
+++ src/components/Map/MapEdgeTooltip.tsx
@@ -11,7 +11,7 @@ export const MapEdgeTooltip = ({ hoverInfo }: IMapEdgeTooltip) => {
 
   return (
     <div
-      className="pointer-events-none absolute z-10 text-center whitespace-nowrap px-2 py-1 default-overlay text-xs text-gray-500"
+      className="pointer-events-none absolute z-10 text-center whitespace-nowrap px-2 py-1 default-overlay text-xs text-gray-500 z-[inherit]"
       style={{ left: x, top: y }}
     >
       <p>SNR: {snr ?? "UNK"}</p>
